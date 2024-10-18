8fdbfa14db678b9e0711a98511c87ad462cc60b5
tracer-ysyx <tracer@ysyx.org>
2024-10-18 19:20:41 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  19:20:41 up 2 days,  9:40,  2 users,  load average: 0.43, 0.52, 0.44

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -228,6 +228,7 @@ word_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
+  scan_expr();
   uint32_t result = eval(0, nr_token - 1);
   *success = true;
   /* TODO: Insert codes to evaluate the expression. */
