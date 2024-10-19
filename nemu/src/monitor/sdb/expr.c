f8080788851a5ebfc8c957e6205f0ae1374f3d80
tracer-ysyx <tracer@ysyx.org>
2024-10-19 14:53:27 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  14:53:27 up 3 days,  5:12,  2 users,  load average: 0.28, 0.37, 0.42

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -129,6 +129,7 @@ static bool make_token(char *e) {
         switch (rules[i].token_type) {
           case TK_INT:
           case TK_HEX:
+          case TK_REG:
             strncpy(tokens[nr_token].str, substr_start, substr_len);
             tokens[nr_token++].type = rules[i].token_type;
             break;
