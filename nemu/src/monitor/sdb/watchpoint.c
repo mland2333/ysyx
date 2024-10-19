4b1fa9a9e9444423f2b48f46fd4d4ad1eabb34da
tracer-ysyx <tracer@ysyx.org>
2024-10-19 17:45:51 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  17:45:51 up 3 days,  8:05,  2 users,  load average: 0.34, 0.47, 0.50

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -23,8 +23,7 @@ static WP *head = NULL, *free_ = NULL;
 void init_wp_pool() {
   int i;
   int j = *(int*)0x1;
-  i = j;
-  for (i = 0; i < NR_WP; i ++) {
+  for (i = j; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
   }
