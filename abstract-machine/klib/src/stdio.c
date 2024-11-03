cb3dad498777cf1a78fb3aaf31cc0d443559c69e
tracer-ysyx <tracer@ysyx.org>
2024-11-03 20:44:45 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.11.5-arch1-1 #1 SMP PREEMPT_DYNAMIC Tue, 22 Oct 2024 18:31:38 +0000 x86_64 GNU/Linux  20:44:45 up 2 days,  1:27,  2 users,  load average: 0.74, 0.98, 1.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -2,6 +2,7 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdarg.h>
+#include <string.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
@@ -10,11 +11,47 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  const char* s = fmt;
+  char* str;
+  char digits[] = "0123456789abcdef";
+  int num;
+  int count = 0;
+  while (*s) {
+    if (*s == '%') {
+      s++;
+      switch (*s) {
+      case 'd':
+        num = va_arg(ap, int);
+        if(num < 0) out[count++] = '-';
+        do {
+          out[count++] = digits[num%10];
+          num /= 10;
+        }while (num != 0);
+        s++;
+        break;
+      case 's':
+          if ((str = va_arg(ap, char*)) == 0) {
+            str = "(null)";
+          }
+          while (*str) {
+            out[count++] = *str++;
+          }
+        break;
+      default:
+          return count;
+      }
+    }
+    else {
+      out[count++] = *s++;
+    }
+  }
+  return count;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  return vsprintf(out, fmt, ap);
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
