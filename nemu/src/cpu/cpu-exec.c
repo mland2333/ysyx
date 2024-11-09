9747f9bcd13a4f15e38f0e2e8292a65559a6a513
tracer-ysyx <tracer@ysyx.org>
2024-11-09 13:54:21 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.11.5-arch1-1 #1 SMP PREEMPT_DYNAMIC Tue, 22 Oct 2024 18:31:38 +0000 x86_64 GNU/Linux  13:54:21 up 7 days, 18:36,  2 users,  load average: 0.57, 0.65, 0.49

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -157,7 +157,8 @@ void cpu_exec(uint64_t n) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
 
     case NEMU_END: case NEMU_ABORT:
-      IFDEF(CONFIG_ITRACE, print_buffer());
+      if (nemu_state.state == NEMU_ABORT)
+        IFDEF(CONFIG_ITRACE, print_buffer());
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
