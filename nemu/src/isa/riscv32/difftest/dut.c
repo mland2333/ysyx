34362a08f753ec9e81fdeaf9e5b4e606e4085b38
tracer-ysyx <tracer@ysyx.org>
2024-11-11 18:32:27 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  18:32:27 up  6:25,  2 users,  load average: 0.46, 0.94, 0.76

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -15,10 +15,17 @@
 
 #include <isa.h>
 #include <cpu/difftest.h>
-#include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  for (int i = 0; i < MUXDEF(CONFIG_RVE, 16, 32); i++) {
+    if (ref_r->gpr[i] != cpu.gpr[i]) {
+      return false;
+    }
+  }
+  if (ref_r->pc != cpu.pc) {
+    return false;
+  }
+  return true;
 }
 
 void isa_difftest_attach() {
