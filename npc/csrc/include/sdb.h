5130360c17dd92217f7374f7ad7c39adf35feb20
tracer-ysyx <tracer@ysyx.org>
2024-11-16 10:24:00 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  10:24:00 up 4 days, 22:17,  2 users,  load average: 0.58, 0.65, 0.57

--- npc/csrc/include/sdb.h
+++ npc/csrc/include/sdb.h
@@ -15,20 +15,41 @@ enum class NPC_STATE{
   QUIT
 };
 class Sdb{
-  std::unordered_map<std::string, std::function<SIM_STATE(Simulator*, char*)>> sdb_map_;
+  std::unordered_map<std::string, std::function<SIM_STATE(Sdb*, char*)>> sdb_map_;
   bool is_batch = false;
   NPC_STATE state = NPC_STATE::RUNNING;
   uint64_t timer = 0;
+  uint64_t inst_nums = 0;
   void statistic();
   uint64_t get_time();
+  Simulator* sim_;
+  Memory* mem_;
+  
 public:
-  Sdb(Args& args) : is_batch(args.is_batch){
+  Sdb(Args& args, Simulator* sim, Memory* mem) : is_batch(args.is_batch), sim_(sim), mem_(mem){
     init();
   }
   void init();
   void welcome();
-  void add_command(const char* command, std::function<SIM_STATE(Simulator*, char*)> func){
+  void add_command(const char* command, std::function<SIM_STATE(Sdb*, char*)> func){
     sdb_map_[command] = func;
   }
-  int run(Simulator* sim_);
+  SIM_STATE exec_once(){
+    inst_nums++;
+    return sim_->exec_once();
+  }
+  SIM_STATE exec(int n);
+  void cpu_display(){
+    sim_->cpu.display();
+  }
+  uint32_t mem_read(uint32_t addr){
+    return mem_->read<uint32_t>(addr);
+  }
+  uint32_t inst_fetch(uint32_t pc){
+    return mem_->read<uint32_t>(pc);
+  }
+  void quit(){
+    sim_->quit();
+  }
+  int run();
 };
