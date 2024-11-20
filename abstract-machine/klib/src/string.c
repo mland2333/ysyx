b52dac301201078513dc0c99fd79d59db93510f4
tracer-ysyx <tracer@ysyx.org>
2024-11-20 15:24:25 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  15:24:25 up 9 days,  3:17,  2 users,  load average: 5.66, 6.06, 5.13

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,7 +5,11 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t i = 0;
+    for(i = 0; *s != '\0'; s++)
+        i++;
+    i++;
+  return i;
 }
 
 char *strcpy(char *dst, const char *src) {
