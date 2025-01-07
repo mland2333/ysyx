c6d13446f087d5deb06c2fcf0ca0beb364280a41
tracer-ysyx <tracer@ysyx.org>
2025-01-07 20:13:22 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.12.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 14:24:37 +0000 x86_64 GNU/Linux  20:13:22 up  9:57,  1 user,  load average: 0.86, 0.78, 0.82

--- abstract-machine/scripts/platform/cachesim.mk
+++ abstract-machine/scripts/platform/cachesim.mk
@@ -32,7 +32,7 @@ image: image-dep
 
 
 run: insert-arg
-	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin CONFIG_CACHESIM=1 2&>1
+	$(MAKE) -s -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin CONFIG_CACHESIM=1
 	$(MAKE) -C $(NPC_HOME)/../cachesim run ITRACE=$(NAME).txt
 	# $(MAKE) -C $(NPC_HOME) ISA=$(ISA) run ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin CONFIG_YSYXSOC=1
 gdb: insert-arg
