857eff303fe3fe9aaf045e7e3a4b49c43097baab
tracer-ysyx <tracer@ysyx.org>
2025-01-07 20:09:51 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.12.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 27 Dec 2024 14:24:37 +0000 x86_64 GNU/Linux  20:09:51 up  9:54,  1 user,  load average: 0.30, 0.72, 0.81

--- npc/vsrc/icache.v
+++ npc/vsrc/icache.v
@@ -1,7 +1,7 @@
 module ysyx_24110006_ICACHE #(
     parameter BLOCK_SIZE = 4,
-    parameter NUM_BLOCKS = 8,
-    parameter NUM_WAYS = 1
+    parameter NUM_BLOCKS = 16,
+    parameter NUM_WAYS = 2
   )(
   input i_clock,
   input i_reset,
@@ -27,31 +27,31 @@ module ysyx_24110006_ICACHE #(
   input i_axi_rlast
 );
 
-/* reg hit_counter; */
-/* reg miss_counter; */
-/* reg [31:0] miss_time; */
-/* always@(posedge i_clock)begin */
-/*   if(i_reset) begin */
-/*     hit_counter <= 0; */
-/*     miss_counter <= 0; */
-/*   end */
-/*   else begin */
-/*     if(state == judge_t && hit) hit_counter <= 1; */
-/*     else if(state == judge_t && !hit) miss_counter <= 1; */
-/*     else begin */
-/*       hit_counter <= 0; */
-/*       miss_counter <= 0; */
-/*     end */
-/*   end */
-/* end */
-/**/
-/* always@(posedge i_clock)begin */
-/*   if(i_reset) miss_time <= 0; */
-/*   else begin */
-/*     if(state == judge_t && !hit || state == axi_t) miss_time <= miss_time+1; */
-/*     else if(o_valid) miss_time <= 0; */
-/*   end */
-/* end */
+reg hit_counter;
+reg miss_counter;
+reg [31:0] miss_time;
+always@(posedge i_clock)begin
+  if(i_reset) begin
+    hit_counter <= 0;
+    miss_counter <= 0;
+  end
+  else begin
+    if(state == judge_t && hit) hit_counter <= 1;
+    else if(state == judge_t && !hit) miss_counter <= 1;
+    else begin
+      hit_counter <= 0;
+      miss_counter <= 0;
+    end
+  end
+end
+
+always@(posedge i_clock)begin
+  if(i_reset) miss_time <= 0;
+  else begin
+    if(state == judge_t && !hit || state == axi_t) miss_time <= miss_time+1;
+    else if(o_valid) miss_time <= 0;
+  end
+end
 
 reg [31:0] pc;
 reg [31:0] inst;
