231a9cfc7241ef02ea76d59ce47bf4b37d7de5c5
tracer-ysyx <tracer@ysyx.org>
2025-01-08 12:16:29 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.12.8-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 02 Jan 2025 22:52:26 +0000 x86_64 GNU/Linux  12:16:29 up  1:18,  1 user,  load average: 1.30, 1.24, 0.96

--- abstract-machine/scripts/platform/cachesim.mk
+++ abstract-machine/scripts/platform/cachesim.mk
@@ -37,10 +37,7 @@ run: insert-arg
 
 cachesim: insert-arg
 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin CONFIG_CACHESIM=1 > /dev/null 2>&1
-	CURRENT_DIR=$(pwd)
-	cd /home/mland/ysyx-workbench/cachesim
-	python main.py $(NAME).txt
-	cd $CURRENT_DIR
+	cd /home/mland/ysyx-workbench/cachesim && python main.py $(NAME).txt
 
 gdb: insert-arg
 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin CONFIG_CACHESIM=1
