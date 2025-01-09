05b22403fc212bfd8894e02e0add1d71c5a85fdd
tracer-ysyx <tracer@ysyx.org>
2025-01-09 14:52:12 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.12.8-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 02 Jan 2025 22:52:26 +0000 x86_64 GNU/Linux  14:52:12 up  1:32,  1 user,  load average: 0.49, 0.45, 0.71

--- npc/vsrc/icache.v
+++ npc/vsrc/icache.v
@@ -1,7 +1,7 @@
 module ysyx_24110006_ICACHE #(
-    parameter BLOCK_SIZE = 16,
     parameter NUM_BLOCKS = 4,
-    parameter NUM_WAYS = 1
+    parameter NUM_WAYS = 1,
+    parameter BLOCK_SIZE = 16
   )(
   input i_clock,
   input i_reset,
