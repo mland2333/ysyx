3671af9714373aa8daa152b119eabbf6dbdc6720
tracer-ysyx <tracer@ysyx.org>
2025-02-11 20:06:45 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.13.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 02 Feb 2025 01:02:29 +0000 x86_64 GNU/Linux  20:06:45 up 4 days, 51 min,  1 user,  load average: 0.75, 0.69, 0.51

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,8 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    default: panic("Unhandled event ID = %d", e.event);
+    case EVENT_YIELD: printf("yield\n");
+    default: 
   }
 
   return c;
