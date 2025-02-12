7bd1569962acdf0d9b697b7aa19eeda973aaa1a7
tracer-ysyx <tracer@ysyx.org>
2025-02-12 20:04:55 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.13.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 02 Feb 2025 01:02:29 +0000 x86_64 GNU/Linux  20:04:55 up 5 days, 49 min,  1 user,  load average: 0.94, 1.45, 1.94

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,32 @@
 #include <common.h>
 #include "syscall.h"
 #include "am.h"
+static const char*syscall_name[] = {
+  "SYS_exit",
+  "SYS_yield",
+  "SYS_open",
+  "SYS_read",
+  "SYS_write",
+  "SYS_kill",
+  "SYS_getpid",
+  "SYS_close",
+  "SYS_lseek",
+  "SYS_brk",
+  "SYS_fstat",
+  "SYS_time",
+  "SYS_signal",
+  "SYS_execve",
+  "SYS_fork",
+  "SYS_link",
+  "SYS_unlink",
+  "SYS_wait",
+  "SYS_times",
+  "SYS_gettimeofday"
+};
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  printf("%s\n", syscall_name[a[0]]);
   switch (a[0]) {
     case 0: halt(c->GPR1); break;
     case 1: yield();break;
