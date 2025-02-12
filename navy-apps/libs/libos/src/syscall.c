294d2729f4c15a68bbed32b324dec59fcba2dde8
tracer-ysyx <tracer@ysyx.org>
2025-02-12 21:44:59 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.13.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 02 Feb 2025 01:02:29 +0000 x86_64 GNU/Linux  21:44:59 up 5 days,  2:29,  1 user,  load average: 0.80, 1.54, 1.65

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -68,11 +68,11 @@ int _open(const char *path, int flags, mode_t mode) {
 int _write(int fd, void *buf, size_t count) {
   return _syscall_(SYS_write, fd, (intptr_t)buf, count);
 }
-
-void *_sbrk(intptr_t increment) {
   extern char _end[];
   static intptr_t msbrk;
   static int first_sbrk = 1;
+void *_sbrk(intptr_t increment) {
+
   if(first_sbrk == 1) {
     msbrk = (intptr_t) _end;
     first_sbrk = 0;
