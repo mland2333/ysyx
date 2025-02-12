0cf9f785f0000a29049ab750b5a25cda606a9d68
tracer-ysyx <tracer@ysyx.org>
2025-02-12 18:18:42 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.13.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 02 Feb 2025 01:02:29 +0000 x86_64 GNU/Linux  18:18:42 up 4 days, 23:03,  1 user,  load average: 0.32, 0.45, 0.56

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,6 +3,7 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD: printf("yield\n");
+    case EVENT_SYSCALL: ;
     default: printf("%d\n", e.event);
   }
 
