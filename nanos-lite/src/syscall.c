3c50c436f4b809d9d759d6f22be0da29a83d5fe9
tracer-ysyx <tracer@ysyx.org>
2025-02-12 20:34:37 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.13.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 02 Feb 2025 01:02:29 +0000 x86_64 GNU/Linux  20:34:37 up 5 days,  1:19,  1 user,  load average: 1.32, 1.49, 1.61

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,28 +2,28 @@
 #include <stdint.h>
 #include "syscall.h"
 #include "am.h"
-static const char*syscall_name[] = {
-  "SYS_exit",
-  "SYS_yield",
-  "SYS_open",
-  "SYS_read",
-  "SYS_write",
-  "SYS_kill",
-  "SYS_getpid",
-  "SYS_close",
-  "SYS_lseek",
-  "SYS_brk",
-  "SYS_fstat",
-  "SYS_time",
-  "SYS_signal",
-  "SYS_execve",
-  "SYS_fork",
-  "SYS_link",
-  "SYS_unlink",
-  "SYS_wait",
-  "SYS_times",
-  "SYS_gettimeofday"
-};
+/* static const char*syscall_name[] = { */
+/*   "SYS_exit", */
+/*   "SYS_yield", */
+/*   "SYS_open", */
+/*   "SYS_read", */
+/*   "SYS_write", */
+/*   "SYS_kill", */
+/*   "SYS_getpid", */
+/*   "SYS_close", */
+/*   "SYS_lseek", */
+/*   "SYS_brk", */
+/*   "SYS_fstat", */
+/*   "SYS_time", */
+/*   "SYS_signal", */
+/*   "SYS_execve", */
+/*   "SYS_fork", */
+/*   "SYS_link", */
+/*   "SYS_unlink", */
+/*   "SYS_wait", */
+/*   "SYS_times", */
+/*   "SYS_gettimeofday" */
+/* }; */
 
 void sys_write(Context *c){
   char* buf = (char*)c->gpr[11];
@@ -35,7 +35,7 @@ void sys_write(Context *c){
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  printf("%s\n", syscall_name[a[0]]);
+  /* printf("%s\n", syscall_name[a[0]]); */
   switch (a[0]) {
     case 0: halt(c->GPR1); break;
     case 1: yield();break;
