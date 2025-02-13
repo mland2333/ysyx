bd827db173031d9ccbba4cdc978db7e021a4c793
tracer-ysyx <tracer@ysyx.org>
2025-02-13 19:22:39 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.13.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 02 Feb 2025 01:02:29 +0000 x86_64 GNU/Linux  19:22:39 up 6 days, 7 min,  1 user,  load average: 1.15, 1.03, 0.91

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -32,7 +32,7 @@ extern size_t dispinfo_read(void *buf, size_t offset, size_t len);
 static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
 #include "files.h"
   {"/dev/event", 0, 0, events_read, invalid_write},
   {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
