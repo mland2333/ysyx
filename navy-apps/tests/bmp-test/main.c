2681789b76cc24d602a823600c5849d77e327d2a
tracer-ysyx <tracer@ysyx.org>
2025-02-13 19:19:10 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.13.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 02 Feb 2025 01:02:29 +0000 x86_64 GNU/Linux  19:19:10 up 6 days, 3 min,  1 user,  load average: 0.99, 1.09, 0.89

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -7,7 +7,8 @@
 int main() {
   NDL_Init(0);
   int w, h;
-  printf("hello\n");
+  extern char _end[];
+  printf("end = 0x%x", (intptr_t)_end);
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
