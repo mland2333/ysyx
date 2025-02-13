759ffe2f0c6089c81423e873810a803d38a069eb
tracer-ysyx <tracer@ysyx.org>
2025-02-13 16:48:50 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.13.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 02 Feb 2025 01:02:29 +0000 x86_64 GNU/Linux  16:48:50 up 5 days, 21:33,  1 user,  load average: 0.48, 0.64, 0.68

--- navy-apps/tests/timer-test/timer.c
+++ navy-apps/tests/timer-test/timer.c
@@ -1,14 +1,23 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <sys/time.h>
+/* #include <sys/time.h> */
+#include <NDL.h>
 int main() {
-  struct timeval st;
+  /* struct timeval st; */
+  intptr_t old = 0;
   intptr_t now = 0;
-  while (1) {
-    gettimeofday(&st, NULL);
-    if (st.tv_usec - now >= 500000) {
+  /* while (1) { */
+  /*   gettimeofday(&st, NULL); */
+  /*   if (st.tv_usec - now >= 500000) { */
+  /*     printf("timer\n"); */
+  /*     now = st.tv_usec; */
+  /*   } */
+  /* } */
+  while(1){
+    now = NDL_GetTicks();
+    if (now - old >= 5000){
       printf("timer\n");
-      now = st.tv_usec;
+      old = now;
     }
   }
   
