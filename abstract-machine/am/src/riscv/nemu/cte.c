59b70c3f50f476e0daa303cb049da769d7f1b8f2
tracer-ysyx <tracer@ysyx.org>
2025-02-13 14:05:36 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.13.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 02 Feb 2025 01:02:29 +0000 x86_64 GNU/Linux  14:05:36 up 5 days, 18:49,  1 user,  load average: 0.80, 0.85, 0.89

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -10,7 +10,8 @@ Context* __am_irq_handle(Context *c) {
     switch (c->mcause) {
       case 11: 
         if(c->GPR1 == -1) ev.event = EVENT_YIELD; 
-        else ev.event = EVENT_SYSCALL;
+        /* else ev.event = EVENT_SYSCALL; */
+        else ev.event = EVENT_ERROR;
         c->mepc += 4; 
       break;
       default: ev.event = EVENT_ERROR; break;
