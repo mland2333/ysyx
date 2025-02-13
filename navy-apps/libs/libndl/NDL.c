626ca3b7bd495fe770fe91d842e638d4ed19fc30
tracer-ysyx <tracer@ysyx.org>
2025-02-13 21:12:37 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.13.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 02 Feb 2025 01:02:29 +0000 x86_64 GNU/Linux  21:12:37 up 6 days,  1:57,  1 user,  load average: 0.43, 0.55, 0.67

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -61,7 +61,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   }
   close(fd);
   fd = open("/dev/fb", 0);
-  for (int i = 0; i<h; i++){
+  for (int i = 0; i<h && i < height; i++){
     lseek(fd, i*width*4, SEEK_SET);
     write(fd, (void*)(pixels + i*w), w*sizeof(int));
   }
