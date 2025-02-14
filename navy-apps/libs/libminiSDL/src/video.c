4f647dac6d9ede70903f8dca14455f844e8aa9be
tracer-ysyx <tracer@ysyx.org>
2025-02-14 20:49:40 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.13.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 02 Feb 2025 01:02:29 +0000 x86_64 GNU/Linux  20:49:40 up 7 days,  1:34,  1 user,  load average: 0.66, 0.71, 0.77

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -9,7 +9,7 @@
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
   assert(dst && src);
   assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
-  printf("enter blit\n");
+  /* printf("enter blit\n"); */
   int bytes_per_pixel = src->format->BytesPerPixel;
 
   // 确定源和目标的拷贝区域
@@ -25,10 +25,10 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   for (int i = 0; i < src_h; i++) {
     uint8_t* src_pixel = src->pixels + (src_y + i) * src_w * bytes_per_pixel + src_x * bytes_per_pixel;
     uint8_t* dst_pixel = dst->pixels + (src_y + i) * src_w * bytes_per_pixel + src_x * bytes_per_pixel;
-    printf("dst = %x, src = %x\n", dst_pixel, src_pixel);
+    /* printf("dst = %x, src = %x\n", dst_pixel, src_pixel); */
     memcpy(dst_pixel, src_pixel, src_w * bytes_per_pixel);
   }
-  printf("quit blit\n");
+  /* printf("quit blit\n"); */
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
