54bbe9f75888fbc4c079ba15f696b88f65fa96f9
tracer-ysyx <tracer@ysyx.org>
2025-02-14 12:22:15 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.13.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 02 Feb 2025 01:02:29 +0000 x86_64 GNU/Linux  12:22:15 up 6 days, 17:06,  1 user,  load average: 0.93, 1.14, 0.98

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -25,7 +25,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if (ev.keycode == AM_KEY_NONE) return 0;
   if (ev.keydown) sprintf(buf, "kd %c", ev.keycode);
-  else sprintf(buf, "ku %s", keyname[ev.keycode]);
+  else sprintf(buf, "ku %c", ev.keycode);
   return 3 + sizeof(keyname[ev.keycode]);
 }
 static int width;
