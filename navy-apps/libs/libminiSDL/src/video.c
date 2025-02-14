1c88811b2263c11ab3b6f1fbf244181c0784faba
tracer-ysyx <tracer@ysyx.org>
2025-02-14 20:35:21 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.13.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 02 Feb 2025 01:02:29 +0000 x86_64 GNU/Linux  20:35:21 up 7 days,  1:19,  1 user,  load average: 0.78, 0.88, 0.84

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -23,9 +23,9 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   uint8_t* srcpixel = src->pixels;
   uint8_t* dstpixel = dst->pixels;
   for (int i = 0; i < src_h; i++) {
-    uint8_t* src_pixel = srcpixels + (src_y + i) * src_w * bytes_per_pixel + src_x * bytes_per_pixel;
-    uint8_t *dst_pixel = dstpixels + (src_y + i) * src_w * bytes_per_pixel + src_x * bytes_per_pixel;
-    printf("dst = %p, src = %p\n", dst_pixel, src_pixel);
+    uint8_t* src_pixel = srcpixel + (src_y + i) * src_w * bytes_per_pixel + src_x * bytes_per_pixel;
+    uint8_t* dst_pixel = dstpixel + (src_y + i) * src_w * bytes_per_pixel + src_x * bytes_per_pixel;
+    printf("dst = %x, src = %x\n", dst_pixel, src_pixel);
     memcpy(dst_pixel, src_pixel, src_w * bytes_per_pixel);
   }
   printf("quit blit\n");
