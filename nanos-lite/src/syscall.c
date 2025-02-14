b2cbb5140616700588235926aab8801aa4cf7192
tracer-ysyx <tracer@ysyx.org>
2025-02-14 12:21:07 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.13.1-arch1-1 #1 SMP PREEMPT_DYNAMIC Sun, 02 Feb 2025 01:02:29 +0000 x86_64 GNU/Linux  12:21:07 up 6 days, 17:05,  1 user,  load average: 1.79, 1.29, 1.02

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -68,7 +68,7 @@ int sys_gettimeofday(Context* c){
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  if (true) {
+  if (false) {
     printf("%s ", syscall_name[a[0]]);
     if (a[0] == SYS_open)
       printf("%s", (char*)c->GPR2);
