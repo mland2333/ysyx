49ef6a0512462f0aadd60e3ae8aca6dda5d7f645
mland2333 <2892912401@qq.com>
2025-05-19 21:19:26 +0800

    更改文件结构

--- npc/vsrc/axi/axi_full.sv
+++
@@ -1,9 +0,0 @@
-module ysyx_24110006_AXI_FULL (
-    if_axi_rq.slave i_rq,
-    if_axi_full.out o_axi
-);
-
-
-
-
-endmodule
--- npc/vsrc/interface/if_fencei.sv
+++
@@ -1,6 +0,0 @@
-interface if_fencei ();
-  logic rq;
-  logic fin;
-  modport master(output rq, input fin);
-  modport slave(input rq, output fin);
-endinterface
