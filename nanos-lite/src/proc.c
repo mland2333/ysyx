32f6061b43ed39d51b3d53a63a2baa81de022061
tracer-ysyx <tracer@ysyx.org>
2025-05-30 21:07:38 +0800

    > compile NEMU ysyx_22040000 毛乐 Linux arch 6.14.7-arch2-1 #1 SMP PREEMPT_DYNAMIC Thu, 22 May 2025 05:37:49 +0000 x86_64 GNU/Linux  21:07:38 up  5:50,  1 user,  load average: 1.60, 1.85, 2.37

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -21,7 +21,7 @@ void hello_fun(void *arg) {
 extern void naive_uload(PCB *pcb, const char *filename);
 void init_proc() {
   switch_boot_pcb();
-  naive_uload(NULL, "/bin/nslider");
+  naive_uload(NULL, "/bin/timer-test");
 
   Log("Initializing processes...");
 
